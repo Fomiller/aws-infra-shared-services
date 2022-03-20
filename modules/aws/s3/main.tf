0e34367bf22ff09926f8cddab205bd63fbc2e004
Forrest Miller <forrestmillerj@gmail.com>
2022-03-20 17:35:17 -0500

    s3 object lock set false

--- modules/aws/s3/main.tf
+++ modules/aws/s3/main.tf
@@ -1,6 +1,9 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "fomiller-dev"
 
+  object_lock_configuration {
+    object_lock_enabled = false
+  }
   tags = {
     Owner       = "Forrest Miller"
     Email       = "forrestmillerj@gmail.com"
