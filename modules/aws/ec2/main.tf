c5385514bf0b96ab771da667e41c582faa0d3999
Forrest Miller <forrestmillerj@gmail.com>
2022-03-20 16:17:33 -0500

    edit

--- modules/aws/ec2/main.tf
+++ modules/aws/ec2/main.tf
@@ -6,13 +6,3 @@ resource "aws_instance" "app_server" {
     Name = "RemoteStateInstance"
   }
 }
-
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
-}
