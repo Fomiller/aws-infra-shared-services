bd2a6148fec4e4208db631fb7ffb36fa5ea8bdff
Forrest Miller <forrestmillerj@gmail.com>
2023-11-23 08:53:21 -0600

    fix source for archive_file

--- infra/modules/aws/lambda/_data.tf
+++ infra/modules/aws/lambda/_data.tf
@@ -1,6 +1,6 @@
 data "archive_file" "zip" {
   type        = "zip"
-  source_file = "${path.module}/src/bin/lambda_hello"
+  source_file = "${path.module}/src/bin/lambda_congocoon"
   output_path = "${path.module}/lambda_function.zip"
 }
 
