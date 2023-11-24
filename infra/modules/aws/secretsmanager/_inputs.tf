e30e8723c09e85300135b927424cfd04d69727c1
Forrest Miller <forrestmillerj@gmail.com>
2023-11-24 10:18:31 -0600

    migrating terraform creds

---
+++ infra/modules/aws/secretsmanager/_inputs.tf
@@ -0,0 +1,4 @@
+variable "kms_key_arn_master" {
+  description = "Fomiller master kms key arn"
+  type        = string
+}
--- infra/modules/aws/secretsmanager/secretsmanager.tf
+++ infra/modules/aws/secretsmanager/secretsmanager.tf
@@ -8,4 +8,17 @@ resource "aws_secretsmanager_secret_version" "gmail_api_key" {
   secret_string = var.gmail_api_key
 }
 
+resource "aws_secretsmanager_secret" "deployer_creds" {
+  name       = "${var.namespace}-${var.environment}-terraform-deployer-creds"
+  kms_key_id = var.kms_key_arn_master
+}
+
+resource "aws_secretsmanager_secret_version" "deployer_creds" {
+  secret_id = aws_secretsmanager_secret.deployer_creds.id
+  secret_string = jsonencode(tomap({
+    "access_key_id"     = var.terraform_deployer_user,
+    "secret_access_key" = var.terraform_deployer_pass
+    }
+  ))
+}
 
