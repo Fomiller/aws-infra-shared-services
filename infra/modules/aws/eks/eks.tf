9a28bb214288b8b8e2b100aaab18da081e00c401
Forrest Miller <forrestmillerj@gmail.com>
2023-12-02 22:17:50 -0600

    update eks fargate profile namespace

--- infra/modules/aws/eks/eks.tf
+++ infra/modules/aws/eks/eks.tf
@@ -12,12 +12,15 @@ resource "aws_eks_cluster" "cluster" {
 
 }
 
-resource "aws_eks_fargate_profile" "fomiller" {
+
+resource "aws_eks_fargate_profile" "kube_system" {
   cluster_name           = aws_eks_cluster.cluster.name
-  fargate_profile_name   = var.namespace
+  fargate_profile_name   = "kube-system"
   pod_execution_role_arn = var.iam_role_arn_eks_fargate_profile
 
+  subnet_ids = var.subnet_ids_private
+
   selector {
-    namespace = var.namespace #this might need to be moved to chat-stat repo
+    namespace = "kube-system" # this might need to be moved to chat-stat repo
   }
 }
