26c63d2313a58671966fe908815defd2bd592514
Forrest Miller <forrestmillerj@gmail.com>
2023-12-11 11:53:51 -0600

    fomillercloud cert init

---
+++ infra/modules/aws/acm/_inputs.tf
@@ -0,0 +1,2 @@
+variable "route53_zone_name_fomillercloud_subdomain_public" { type = string }
+variable "route53_zone_id_fomillercloud_subdomain_public" { type = string }
---
+++ infra/modules/aws/route53/_outputs.tf
@@ -0,0 +1,7 @@
+output "route53_zone_name_fomillercloud_subdomain_public" {
+  value = aws_route53_zone.fomillercloud_subdomain_public.name
+}
+
+output "route53_zone_id_fomillercloud_subdomain_public" {
+  value = aws_route53_zone.fomillercloud_subdomain_public.zone_id
+}
