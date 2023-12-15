c6b68cfa366ce433b77302bbb8fb9de7e5116df1
Forrest Miller <forrestmillerj@gmail.com>
2023-12-15 09:27:52 -0600

    init security module

---
+++ infra/modules/aws/security/security.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "vpc_public_ingress" {
+  name        = "${var.namespace} VPC Public Ingress SG"
+  description = "Allow incoming HTTP and HTTPS traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic from all sources
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic from all sources
+  }
+}
+
