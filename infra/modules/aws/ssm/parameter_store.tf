a442254cfea87f7ab1b387a38b4c845a9c36ce43
Forrest Miller <forrestmillerj@gmail.com>
2023-12-27 15:47:10 -0600

    fix param name

--- infra/modules/aws/ssm/parameter_store.tf
+++ infra/modules/aws/ssm/parameter_store.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "elasticache_cluster_configuration_endpoint_redis" {
-  name  = "${var.environment}/${var.namespace}-redis-configuration-endpoint"
+  name  = "/${var.environment}/${var.namespace}-redis-configuration-endpoint"
   type  = "String"
   value = var.elasticache_cluster_configuration_endpoint_redis
 }
