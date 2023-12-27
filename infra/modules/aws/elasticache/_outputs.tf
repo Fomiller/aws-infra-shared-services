fe63ab60f88bf620fc86e6e8a9d14810089cefd4
Forrest Miller <forrestmillerj@gmail.com>
2023-12-27 16:00:51 -0600

    fix elasticache output

--- infra/modules/aws/elasticache/_outputs.tf
+++ infra/modules/aws/elasticache/_outputs.tf
@@ -1,3 +1,3 @@
 output "elasticache_cluster_configuration_endpoint_redis" {
-  value = aws_elasticache_cluster.redis.configuration_endpoint
+  value = aws_elasticache_cluster.redis.cache_nodes[0].address
 }
