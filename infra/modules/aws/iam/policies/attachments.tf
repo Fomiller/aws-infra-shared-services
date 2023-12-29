a82c83554c196a43a915c122228fdc44ce517f9c
Forrest Miller <forrestmillerj@gmail.com>
2023-12-29 05:51:44 -0600

    eks policies

--- infra/modules/aws/iam/policies/attachments.tf
+++ infra/modules/aws/iam/policies/attachments.tf
@@ -17,3 +17,8 @@ resource "aws_iam_role_policy_attachment" "eks_fargate_profile_logging" {
   role       = var.iam_role_name_eks_fargate_profile
   policy_arn = aws_iam_policy.eks_logging.arn
 }
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_ec2_read" {
+  role       = var.iam_role_name_eks_cluster
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
