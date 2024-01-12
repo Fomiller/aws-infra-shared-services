243ffc5604b5583e829f40754ec707d88ddace02
Forrest Miller <forrestmillerj@gmail.com>
2024-01-12 05:09:31 -0600

    rename timestream resource

--- infra/modules/aws/timestream/timestream.tf
+++ infra/modules/aws/timestream/timestream.tf
@@ -1,4 +1,4 @@
-resource "aws_timestreamwrite_database" "example" {
+resource "aws_timestreamwrite_database" "fomiller" {
   database_name = var.namespace
   kms_key_id    = var.kms_key_arn_master
 }
