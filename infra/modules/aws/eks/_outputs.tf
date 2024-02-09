a02b0587f6f068e4a7b59f4ee5b2dc91fc0ad3f6
Forrest Miller <forrestmillerj@gmail.com>
2024-02-09 16:59:40 -0600

    fix karpenter iam role names

--- infra/modules/aws/eks/_outputs.tf
+++ infra/modules/aws/eks/_outputs.tf
@@ -6,6 +6,22 @@ output "eks_cluster_default_security_group_id" {
   value = aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id
 }
 
-# output "eks_vpc" {
-#   value = aws_eks_cluster.cluster.vpc_config[0]
-# }
+output "cluster_name" {
+  value = aws_eks_cluster.cluster.name
+}
+
+output "cluster_id" {
+  value = aws_eks_cluster.cluster.id
+}
+
+output "karpenter_controller_name" {
+  value = module.karpenter.iam_role_name
+}
+
+output "karpenter_node_name" {
+  value = module.karpenter.node_iam_role_name
+}
+
+output "karpenter_iam_unique" {
+  value = module.karpenter.iam_role_unique_id
+}
--- infra/modules/aws/eks/karpenter.tf
+++ infra/modules/aws/eks/karpenter.tf
@@ -1,6 +1,7 @@
 module "karpenter" {
-  source = "terraform-aws-modules/eks/aws//modules/karpenter"
-
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "20.2.1"
+  
   cluster_name = aws_eks_cluster.cluster.name
 
   enable_irsa                     = true
@@ -9,6 +10,9 @@ module "karpenter" {
 
   create_iam_role   = true
   create_node_iam_role = true
+  iam_role_use_name_prefix = false
+  node_iam_role_use_name_prefix = false
+  iam_role_name = "KarpenterController-${aws_eks_cluster.cluster.name}"
   node_iam_role_name = "KarpenterNodeRole-${aws_eks_cluster.cluster.name}"
   # node_iam_role_arn = var.iam_role_arn_eks_node_groups
 }
