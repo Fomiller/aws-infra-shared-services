c7fd5eeaf18e0a90171d70ff55fb42cc0e2fec1f
Forrest Miller <forrestmillerj@gmail.com>
2024-02-14 19:52:31 -0600

    fix kms policy

--- infra/modules/aws/kms/kms.tf
+++ infra/modules/aws/kms/kms.tf
@@ -6,19 +6,41 @@ resource "aws_kms_key" "master" {
 resource "aws_kms_key_policy" "master" {
   key_id = aws_kms_key.master.id
   policy = jsonencode({
-    Id = "master"
-    Statement = [
+    "Id" : "master",
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        Sid    = "Enable IAM User Permissions"
-        Action = "kms:*"
-        Effect = "Allow"
+        "Sid" : "Enable IAM User Permissions",
+        "Action" : "kms:*",
+        "Effect" : "Allow",
         "Principal" : {
-          "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${title(var.namespace)}*"
+          "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
         },
-        Resource = "*"
+        "Resource" : "*"
       },
+      {
+        "Sid" : "Allow use of the key",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "*"
+        },
+        "Action" : [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        "Condition" : {
+          "ArnLike" : {
+            "aws:PrincipalArn" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${title(var.namespace)}*"
+          }
+        },
+        "Resource" : "*"
+      }
     ]
-  })
+    }
+  )
 }
 
 resource "aws_kms_alias" "master" {
